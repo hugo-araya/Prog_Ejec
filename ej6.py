42f711c522804131a974450aad4a631f9cf2a220
Hugo Araya C <42989121+hugo-araya@users.noreply.github.com>
2023-09-08 09:52:32 -0300

    n

---
+++ ej6.py
@@ -0,0 +1,5 @@
+cadena=input("Ingresa un texto:")
+print("El caracter en primer lugar es:", cadena[0]) 
+print("Ingresa un numero positivo menor a", len(cadena)) 
+indice = int(input())
+print("El caracter en esa posición es:", cadena[indice])
